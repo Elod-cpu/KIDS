b3156f00ab9a6433c3f0c2bb35305a44bea23111
Elod-cpu <oulendoeloise@gmail.com>
2022-08-29 14:04:13 +0100

    second push

--- index.js
+++ index.js
@@ -6,18 +6,3 @@ const toggle = document.getElementById ('toggle');
             sidebar.classList.toggle('active');
         }
 
-function rest() {
-    event.preventDefault();
-    let parent = document.getElementById("parent").value;
-    let mail = document.getElementById("mail").value;
-    let enfant = document.getElementById("enfant").value;
-    let classe = document.getElementById("classe").value;
-    
-   localStorage('ls_parent', parent);
-   localStorage('ls_mail', mail);
-   localStorage('ls_enfant', enfant);
-   localStorage('ls_classe', classe);
-};
-
-
-   
